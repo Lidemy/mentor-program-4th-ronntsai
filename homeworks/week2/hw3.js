0a2c6f6ba0c6e0221468bbbbe324abd763e263d8
ronn tsai <ronntsai@gmail.com>
2020-06-25 00:38:38 +0800

    week2

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,12 @@
 function reverse(str) {
+  let ans = '';
+  for(let i = str.length - 1; i >= 0; i--) {
+    ans += str[i];
+  }
+  console.log(ans);
   
 }
 
 reverse('hello');
+reverse('yoyoyo');
+reverse('1,2,3,2,1')
